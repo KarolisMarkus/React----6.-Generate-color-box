9c917ef0f04abb20ca4ede53123a0aa9674bf37c
karolis_markus <karolis.markuskis@gmail.com>
2019-11-22 23:45:53 +0200

    React tutorial exercise number 6

---
+++ src/BoxList.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+class BoxList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boxes: []
+    };
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+
+  create(newBox) {
+    this.setState({
+      boxes: [...this.state.boxes, newBox]
+    });
+  }
+
+  remove(id) {
+    this.setState({
+      boxes: this.state.boxes.filter(box => box.id !== id)
+    });
+  }
+
+  render() {
+    const boxes = this.state.boxes.map(box => (
+      <Box
+        key={box.id}
+        id={box.id}
+        width={box.width}
+        height={box.height}
+        color={box.color}
+        removeBox={() => this.remove(box.id)}
+      />
+    ));
+    return (
+      <div>
+        <h1>This is some boxes:</h1>
+        <h2>Add a box:</h2>
+        <NewBoxForm createBox={this.create} />
+        {boxes}
+      </div>
+    );
+  }
+}
+
+export default BoxList;
